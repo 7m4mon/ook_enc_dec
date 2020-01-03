293f6f8eeb0c39f26a6be6ec6aa5db1b2279f0cb
7m4mon <7m4mon@gmail.com>
2020-01-03 11:43:46 +0900

    deleted old module files

--- gr-mascon__decode_module/build/apps/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nomura/pla_hikari_cont/gr-mascon__decode_module/apps
-# Build directory: /home/nomura/pla_hikari_cont/gr-mascon__decode_module/build/apps
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
