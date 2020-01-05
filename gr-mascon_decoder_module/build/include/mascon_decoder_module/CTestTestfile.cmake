05c593e3ad221b40d89faddea0ca0b1285329e83
7m4mon <7m4mon@gmail.com>
2020-01-05 23:02:18 +0900

    deleted "build" directory.

--- gr-mascon_decoder_module/build/include/mascon_decoder_module/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nomura/pla_hikari_cont/gr-mascon_decoder_module/include/mascon_decoder_module
-# Build directory: /home/nomura/pla_hikari_cont/gr-mascon_decoder_module/build/include/mascon_decoder_module
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
