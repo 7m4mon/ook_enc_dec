a44d8d05d14b5e4b8d0c8eb3869cf43cb9f1f373
7m4mon <7m4mon@gmail.com>
2020-01-05 23:25:56 +0900

    replaced signature

--- gr-mascon_decoder_module/python/ook_decoder_ii.py
+++ gr-mascon_decoder_module/python/ook_decoder_ii.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # 
-# Copyright 2020 <+YOU OR YOUR COMPANY+>.
+# Copyright 2020 <7m4mon>.
 # 
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
